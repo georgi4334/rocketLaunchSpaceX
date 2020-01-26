b890f8bbb73f223cdf0761cd25baee818c5724f3
Georgi Naydenov <georgi3333@hotmail.com>
2020-01-26 13:57:47 +0200

    merge code

--- script.js
+++ script.js
@@ -7,30 +7,96 @@ fetch(url)
         return response.json();
     })
     .then((data) => {
-        
+
         data.forEach((element, index) => {
             for (let [key, value] of Object.entries(element)) {
                 if (key === 'first_stage') {
                     for (let firstItem in element[key]) {
                         if (firstItem === 'fuel_amount_tons') {
-                            arrFuelTons.push(element[key][firstItem]);
-                            console.log(element[key][firstItem])
+                            arrFuelTons.push(element[key][firstItem] * 1000);
+                            //console.log(element[key][firstItem])
                         }
                     }
 
                 } else if (key === 'second_stage') {
                     for (let secondItem in element[key]) {
                         if (secondItem === 'fuel_amount_tons') {
-                            arrFuelTons.push(element[key][secondItem])
-                            console.log(element[key][secondItem])
+                            arrFuelTons.push(element[key][secondItem] * 1000)
+                            // console.log(element[key][secondItem])
                         }
                     }
                 }
             }
         });
+
+        let [rocket1, rocket11, rocket2, rocket22, rocket3, rocket33, rocket4, rocket44] = arrFuelTons;
+
+
+        const delayRocket1 = ms => new Promise(resolve => setTimeout(resolve, ms));
+        const removeRocket1 = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+        async function rocketOne(first_stage, second_stage) {
+            setTimeout(() => {
+                console.log('first stage rocket 1')
+            }, first_stage)
+            await delayRocket1(first_stage + second_stage);
+            console.log('second stage rocket 1')
+            await removeRocket1(10)
+            console.log('remove rocket')
+            console.log('text for congrats')
+        }
+
+
+        const delayRocket2 = ms => new Promise(resolve => setTimeout(resolve, ms));
+        const removeRocket2 = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+        async function rocketTwo(first_stage, second_stage) {
+            setTimeout(() => {
+                console.log('first stage rocket 2')
+            }, first_stage)
+            await delayRocket2(first_stage + second_stage);
+            console.log('second stage rocket 2')
+            await removeRocket2(10)
+            console.log('remove rocket')
+            console.log('text for congrats')
+        }
+
+
+        const delayRocket3 = ms => new Promise(resolve => setTimeout(resolve, ms));
+        const removeRocket3 = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+        async function rocketThree(first_stage, second_stage) {
+            setTimeout(() => {
+                console.log('first stage rocket 3')
+            }, first_stage)
+            await delayRocket3(first_stage + second_stage);
+            console.log('second stage rocket 3')
+            await removeRocket3(10)
+            console.log('remove rocket')
+            console.log('text for congrats')
+        }
+
+        const delayRocket4 = ms => new Promise(resolve => setTimeout(resolve, ms));
+        const removeRocket4 = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+        async function rocketFour(first_stage, second_stage) {
+            setTimeout(() => {
+                console.log('first stage rocket 4')
+            }, first_stage)
+            await delayRocket4(first_stage + second_stage);
+            console.log('second stage rocket 4')
+            await removeRocket4(10)
+            console.log('remove rocket')
+            console.log('text for congrats')
+        }
+
+      //  console.log(rocketOne(rocket1, rocket11))
+      //  console.log(rocketTwo(rocket2, rocket22))
+       // console.log(rocketThree(rocket3, rocket33))
+       // console.log(rocketFour(rocket4, rocket44))
+
+
     })
     .catch((error) => {
         console.error('Error:', error);
     });
-
-    console.log(arrFuelTons)
